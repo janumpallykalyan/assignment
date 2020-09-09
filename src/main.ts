5fb0570412f9c1c6eb55748149a83d6bf38030d6
Kalyan Janumpally <janumpallykalyan@gmail.com>
2020-09-09 07:03:23 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.connectMicroservice<MicroserviceOptions>({
         transport: Transport.TCP,
-        options: { host: '0.0.0.0', port: 80, retryAttempts: 5, retryDelay: 3000 },
+        options: { host: '0.0.0.0', port: 3000, retryAttempts: 5, retryDelay: 3000 },
     });
 
     await app.startAllMicroservicesAsync();
-    await app.listen(80);
+    await app.listen(3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
